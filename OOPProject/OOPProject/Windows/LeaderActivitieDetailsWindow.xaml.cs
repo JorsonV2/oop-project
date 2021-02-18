25df44dfb36ab3a4c707dde155a9dc74987482d0
JorsonV2 <38647517+JorsonV2@users.noreply.github.com>
2021-02-18 23:30:48 +0100

    Add activitie details window for leader

---
+++ OOPProject/OOPProject/Windows/LeaderActivitieDetailsWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using OOPProject.Db;
+using OOPProject.Db.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OOPProject.Windows
+{
+    /// <summary>
+    /// Interaction logic for LeaderActivitieDetailsWindow.xaml
+    /// </summary>
+    public partial class LeaderActivitieDetailsWindow : Window
+    {
+        private Activitie activitie;
+        private User user;
+        public LeaderActivitieDetailsWindow(Activitie activitie, User user)
+        {
+            InitializeComponent();
+            this.activitie = activitie;
+            this.user = user;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var db = new ActivitiesContext())
+            {
+                userDataGrid.ItemsSource = db.ActivitiesParticipants.Where(ap => ap.ActivitieId == activitie.ActivitieId);
+            }
+        }
+    }
+}
