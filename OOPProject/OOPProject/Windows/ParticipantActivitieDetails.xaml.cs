d05f12ccaf3bff066b2504f24620d887da5c65e6
JorsonV2 <38647517+JorsonV2@users.noreply.github.com>
2021-02-18 22:47:34 +0100

    create activities details for participant

---
+++ OOPProject/OOPProject/Windows/ParticipantActivitieDetails.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OOPProject.Windows
+{
+    /// <summary>
+    /// Interaction logic for ParticipantActivitieDetails.xaml
+    /// </summary>
+    public partial class ParticipantActivitieDetails : Window
+    {
+        public ParticipantActivitieDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
